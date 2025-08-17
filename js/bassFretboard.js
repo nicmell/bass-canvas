2493a26625cc9a1544755cd5c1776b44687c7f93
nicolamelloni <nicola.melloni@slesh.it>
2025-08-17 09:18:36 +0200

    mini refactor

--- js/bassFretboard.js
+++ js/bassFretboard.js
@@ -1,175 +1,180 @@
-import {colors, openNotes} from "./constants";
-import {adaptNotesForScale, noteName} from "./utils";
+import {colors} from "./constants";
+import {noteName} from "./utils";
 
 export class BassFretboard {
-  static canvasWidth = 300;
-  static canvasHeight = 150;
+    static canvasWidth = 300;
+    static canvasHeight = 150;
 
-  constructor({
-                notes,
-                openNotes,
-                scale,
-                startFret,
-                endFret,
-                position,
-                stringCount = 4,
-              }) {
-    const {canvasWidth, canvasHeight} = BassFretboard
+    constructor({
+                    notes,
+                    openNotes,
+                    scale,
+                    startFret,
+                    endFret,
+                    position,
+                    stringCount = 4,
+                }) {
+        const {canvasWidth, canvasHeight} = BassFretboard
 
-    this.notes = notes;
-    this.openNotes = openNotes;
-    this.startFret = startFret;
-    this.endFret = endFret;
-    this.scale = scale;
-    this.stringCount = stringCount;
+        this.notes = notes;
+        this.openNotes = openNotes;
+        this.startFret = startFret;
+        this.endFret = endFret;
+        this.scale = scale;
+        this.stringCount = stringCount;
 
-    this.fretCount = endFret - startFret + 1;
-    this.fretWidth = canvasWidth / this.fretCount;
-    this.fretHeight = canvasHeight / (stringCount + 1);
+        this.fretCount = endFret - startFret + 1;
+        this.fretWidth = canvasWidth / this.fretCount;
+        this.fretHeight = canvasHeight / (stringCount + 1);
 
-    this.emptyFretCount = Math.max(startFret, 1) - startFret
-    this.fretboardOffset = this.fretWidth * this.emptyFretCount
+        this.emptyFretCount = Math.max(startFret, 1) - startFret
+        this.fretboardOffset = this.fretWidth * this.emptyFretCount
 
-    this.position = position
-  }
-
-  calculateNoteOnFret(string, fret) {
-    const {openNotes, notes} = this
-
-    const openNoteIndex = notes.indexOf(openNotes[string - 1]);
-    return notes[(openNoteIndex + fret) % notes.length];
-  }
-
-  drawFretboardBackground(ctx) {
-    const {fretHeight, fretboardOffset} = this
-    const {canvasWidth, canvasHeight} = BassFretboard
-
-    ctx.fillStyle = "#f0e4d7";
-    ctx.fillRect(fretboardOffset, fretHeight, canvasWidth, canvasHeight - fretHeight);
-  }
-
-  drawFretLines(ctx) {
-    const {fretWidth, fretHeight, fretCount, emptyFretCount, fretboardOffset} = this;
-    const {canvasHeight} = BassFretboard
-
-
-    for (let i = emptyFretCount; i <= fretCount; i++) {
-      const x = i * fretWidth;
-      ctx.beginPath();
-      ctx.moveTo(x, fretHeight);
-      ctx.lineTo(x, canvasHeight);
-      ctx.strokeStyle = "#000";
-      ctx.lineWidth = 1;
-      ctx.stroke();
+        this.position = position
     }
-  }
 
-  drawStrings(ctx) {
-    const {stringCount, fretHeight, fretboardOffset} = this;
-    const {canvasWidth} = BassFretboard
+    calculateNoteOnFret(string, fret) {
+        const {openNotes, notes} = this
 
-
-    for (let i = 0; i <= stringCount; i++) {
-      const y = (i + 1) * fretHeight;
-      ctx.beginPath();
-      ctx.moveTo(fretboardOffset, y);
-      ctx.lineTo(canvasWidth, y);
-      ctx.strokeStyle = "#000";
-      ctx.lineWidth = 1;
-      ctx.stroke();
+        const openNoteIndex = notes.indexOf(openNotes[string - 1]);
+        return notes[(openNoteIndex + fret) % notes.length];
     }
-  }
 
-  drawFretMarkers(ctx) {
-    const fretMarkers = [3, 5, 7, 9, 15, 17, 19];
-    const {fretCount, startFret, fretWidth, fretHeight} = this;
-    const {canvasHeight} = BassFretboard
+    drawFretboardBackground(ctx) {
+        const {fretHeight, fretboardOffset} = this
+        const {canvasWidth, canvasHeight} = BassFretboard
 
-    for (let i = 0; i < fretCount; i++) {
-      const fretNum = startFret + i;
-      const x = (i + 0.5) * fretWidth;
-      const y = fretHeight + (canvasHeight - fretHeight) / 2;
-
-      if (fretNum >= 1 && fretMarkers.includes(fretNum)) {
-        ctx.beginPath();
-        ctx.arc(x, y, 8, 0, Math.PI * 2);
-        ctx.fillStyle = "#000";
-        ctx.fill();
-      }
-
-      if (fretNum === 12) {
-        ctx.beginPath();
-        ctx.arc(x, y - 10, 8, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(x, y + 10, 8, 0, Math.PI * 2);
-        ctx.fill();
-      }
+        ctx.fillStyle = "#f0e4d7";
+        ctx.fillRect(fretboardOffset, fretHeight, canvasWidth, canvasHeight - fretHeight);
     }
-  }
 
-  drawFretHeaders(ctx) {
-    const {fretCount, startFret, fretWidth, fretHeight} = this;
-    const headerY = fretHeight / 2;
+    drawFretLines(ctx) {
+        const {fretWidth, fretHeight, fretCount, emptyFretCount} = this;
+        const {canvasHeight} = BassFretboard
 
-    ctx.font = "16px Arial";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#000";
 
-    for (let i = 0; i < fretCount; i++) {
-      const fretNum = startFret + i;
-      const x = (i + 0.5) * fretWidth;
-
-      if (fretNum >= 1) {
-        ctx.fillText(fretNum, x, headerY);
-      }
-    }
-  }
-
-  drawScaleNotes(ctx) {
-    const {stringCount, startFret, fretCount, fretWidth, fretHeight, scale, position} = this;
-
-    ctx.font = "12px Arial";
-    ctx.textAlign = "center";
-
-    for (let string = 1; string <= stringCount; string++) {
-      for (let fret = startFret; fret <= fretCount + startFret - 1; fret++) {
-        const note = this.calculateNoteOnFret(string, fret);
-
-        if (scale.includes(note)) {
-          const x = (fret - startFret + 0.5) * fretWidth;
-          const y = (stringCount - string + 1) * fretHeight + fretHeight / 2;
-
-          ctx.beginPath();
-          ctx.arc(x, y, 12, 0, Math.PI * 2);
-          if (position) {
-            const {light, dark} = colors[position]
-            if (note === scale[0]) {
-              ctx.fillStyle = dark
-            } else {
-              ctx.fillStyle = light
-            }
-          } else {
-            ctx.fillStyle = "#fff";
-          }
-          ctx.fill();
-          ctx.strokeStyle = "#000";
-          ctx.lineWidth = 1;
-          ctx.stroke();
-
-          ctx.fillStyle = "#000";
-          ctx.fillText(noteName(note), x, y + 3);
+        for (let i = emptyFretCount; i <= fretCount; i++) {
+            const x = i * fretWidth;
+            ctx.beginPath();
+            ctx.moveTo(x, fretHeight);
+            ctx.lineTo(x, canvasHeight);
+            ctx.strokeStyle = "#000";
+            ctx.lineWidth = 1;
+            ctx.stroke();
         }
-      }
     }
-  }
 
-  draw(ctx) {
-    this.drawFretboardBackground(ctx);
-    this.drawFretHeaders(ctx);
-    this.drawFretLines(ctx);
-    this.drawStrings(ctx);
-    this.drawFretMarkers(ctx);
-    this.drawScaleNotes(ctx);
-  }
+    drawStrings(ctx) {
+        const {stringCount, fretHeight, fretboardOffset} = this;
+        const {canvasWidth} = BassFretboard
+
+
+        for (let i = 0; i <= stringCount; i++) {
+            const y = (i + 1) * fretHeight;
+            ctx.beginPath();
+            ctx.moveTo(fretboardOffset, y);
+            ctx.lineTo(canvasWidth, y);
+            ctx.strokeStyle = "#000";
+            ctx.lineWidth = 1;
+            ctx.stroke();
+        }
+    }
+
+    drawFretMarkers(ctx) {
+        const fretMarkers = [3, 5, 7, 9, 15, 17, 19];
+        const {fretCount, startFret, fretWidth, fretHeight} = this;
+        const {canvasHeight} = BassFretboard
+
+        for (let i = 0; i < fretCount; i++) {
+            const fretNum = startFret + i;
+            const x = (i + 0.5) * fretWidth;
+            const y = fretHeight + (canvasHeight - fretHeight) / 2;
+
+            if (fretNum >= 1 && fretMarkers.includes(fretNum)) {
+                ctx.beginPath();
+                ctx.arc(x, y, 8, 0, Math.PI * 2);
+                ctx.fillStyle = "#000";
+                ctx.fill();
+            }
+
+            if (fretNum === 12) {
+                ctx.beginPath();
+                ctx.arc(x, y - 10, 8, 0, Math.PI * 2);
+                ctx.fill();
+                ctx.beginPath();
+                ctx.arc(x, y + 10, 8, 0, Math.PI * 2);
+                ctx.fill();
+            }
+        }
+    }
+
+    drawFretHeaders(ctx) {
+        const {fretCount, startFret, fretWidth, fretHeight} = this;
+        const headerY = fretHeight / 2;
+
+        ctx.font = "16px Arial";
+        ctx.textAlign = "center";
+        ctx.fillStyle = "#000";
+
+        for (let i = 0; i < fretCount; i++) {
+            const fretNum = startFret + i;
+            const x = (i + 0.5) * fretWidth;
+
+            if (fretNum >= 1) {
+                ctx.fillText(fretNum, x, headerY);
+            }
+        }
+    }
+
+    drawScaleNotes(ctx) {
+        const {stringCount, startFret, fretCount, fretWidth, fretHeight, scale, position} = this;
+
+        ctx.font = "12px Arial";
+        ctx.textAlign = "center";
+
+        for (let string = 1; string <= stringCount; string++) {
+            for (let fret = startFret; fret <= fretCount + startFret - 1; fret++) {
+                const note = this.calculateNoteOnFret(string, fret);
+
+                if (scale.includes(note)) {
+                    const x = (fret - startFret + 0.5) * fretWidth;
+                    const y = (stringCount - string + 1) * fretHeight + fretHeight / 2;
+
+                    ctx.beginPath();
+                    ctx.arc(x, y, 12, 0, Math.PI * 2);
+                    if (position) {
+                        const {light, dark} = colors[position]
+                        if (note === scale[0]) {
+                            ctx.fillStyle = dark
+                        } else {
+                            ctx.fillStyle = light
+                        }
+                    } else {
+                        ctx.fillStyle = "#fff";
+                    }
+                    ctx.fill();
+                    ctx.strokeStyle = "#000";
+                    ctx.lineWidth = 1;
+                    ctx.stroke();
+
+                    ctx.fillStyle = "#000";
+                    ctx.fillText(noteName(note), x, y + 3);
+                }
+            }
+        }
+    }
+
+    draw(ctx) {
+        this.drawFretboardBackground(ctx);
+        this.drawFretHeaders(ctx);
+        this.drawFretLines(ctx);
+        this.drawStrings(ctx);
+        this.drawFretMarkers(ctx);
+        this.drawScaleNotes(ctx);
+    }
+
+    static draw(ctx, options) {
+        (new BassFretboard(options)).draw(ctx);
+    }
+
 }
--- js/index.js
+++ js/index.js
@@ -1,51 +1,53 @@
-import {flatNotes, flatScales, flatScaleConfigurations, sharpScaleConfigurations, positions} from "./constants";
+import {flatScaleConfigurations, sharpScaleConfigurations, positions} from "./constants";
 import {BassFretboard} from "./bassFretboard";
 import {noteName} from "./utils";
 
 function createBassFretboard(configurations, scaleKey, index) {
-  const { canvasWidth, canvasHeight} = BassFretboard
-  const {scale, notes, openNotes, positions} = configurations[scaleKey]
-  const {startFret, endFret, position} = positions[index]
+    const {scale, notes, openNotes, positions} = configurations[scaleKey]
+    const {startFret, endFret, position} = positions[index]
 
-  const canvasElement = document.createElement("canvas")
-  canvasElement.id = [scaleKey, position].join("-")
-  canvasElement.width = canvasWidth
-  canvasElement.height = canvasHeight
+    const canvasElement = document.createElement("canvas")
+    const {canvasWidth, canvasHeight} = BassFretboard
+    canvasElement.id = [scaleKey, position].join("-")
+    canvasElement.width = canvasWidth
+    canvasElement.height = canvasHeight
 
-  const fretboard = new BassFretboard({
-    scale,
-    notes,
-    openNotes,
-    startFret,
-    endFret,
-    position
-  });
-  fretboard.draw(canvasElement.getContext("2d"))
-  return canvasElement
+    BassFretboard.draw(
+        canvasElement.getContext("2d"),
+        {
+            scale,
+            notes,
+            openNotes,
+            startFret,
+            endFret,
+            position
+        })
+
+    return canvasElement
 }
 
 function appendScalesHead(parent) {
-  const {id: scaleKey} = parent
-  const span = document.createElement("span")
-  span.innerText = noteName(scaleKey);
-  parent.append(span);
+    const {id: scaleKey} = parent
+    const span = document.createElement("span")
+    span.innerText = noteName(scaleKey);
+    parent.append(span);
 }
 
 function addendScales(parent, configurations) {
-  const {id: scaleKey} = parent
-  Object.keys(positions).forEach((_, index) => {
-    parent.append(createBassFretboard(configurations, scaleKey, index));
-  })
+    const {id: scaleKey} = parent
+    Object.keys(positions).forEach((_, index) => {
+        parent.append(createBassFretboard(configurations, scaleKey, index));
+    })
 }
 
 document.querySelectorAll(".scale.flat")
-  .forEach((parent) => {
-    appendScalesHead(parent);
-    addendScales(parent, flatScaleConfigurations);
-  });
+    .forEach((parent) => {
+        appendScalesHead(parent);
+        addendScales(parent, flatScaleConfigurations);
+    });
 
 document.querySelectorAll(".scale.sharp")
-  .forEach((parent) => {
-    appendScalesHead(parent);
-    addendScales(parent, sharpScaleConfigurations);
-  });
+    .forEach((parent) => {
+        appendScalesHead(parent);
+        addendScales(parent, sharpScaleConfigurations);
+    });
